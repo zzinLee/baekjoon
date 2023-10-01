65742220fd11911ed7fd0ce427f295f868267a04
zzinLee <91821887+zzinLee@users.noreply.github.com>
2023-10-01 23:30:11 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,5 @@
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.text';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+console.log(input[0].toString().charCodeAt());
