50741cef809219120269c3849721831ec7113d40
zzinLee <91821887+zzinLee@users.noreply.github.com>
2023-10-09 10:01:43 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.js
@@ -0,0 +1,7 @@
+const { log } = require('console');
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.text';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const n = +input[0];
+log(4*n);
