4b57ce564e5bd1fa1429f10b15c731cf1938a0ea
zzinLee <91821887+zzinLee@users.noreply.github.com>
2023-10-25 11:01:33 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 896 ms, Memory: 72408 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let [N, ...dots] = input;
+dots = dots.map((dot) => dot.split(' ').map(Number));
+
+dots.sort(([Ax ,Ay], [Bx, By]) => {
+  return Ay === By ? Ax - Bx : Ay - By;
+})
+
+console.log(dots.map(([x, y]) => `${x} ${y}`).join("\n"));
