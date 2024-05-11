a2c66e0f1e1c27c4cef9f4b747f46a556b09ee62
Sujin Lee <91821887+zzinLee@users.noreply.github.com>
2024-05-11 17:44:35 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 49.10 ms, Memory: 47.2 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -1,12 +1,17 @@
 function solution(land) {
     const n = land.length;
     const m = land[0].length;
+    
+    //방문기록
     const VISIT = "VISITED";
     const visited = land.map((row) => new Array(row.length).fill(0));
+    
+    //DFS
     const moveX = [-1, 1, 0, 0];
     const moveY = [0, 0, -1, 1];
-    const oilAreaAndCoords = {};
-    const q = [];
+    const stack = [];
+    
+    //coord에 해당하는 oilArea
     const oilAreaByCoords = {};
     
     for (let x = 0; x < n; x++) {
@@ -16,13 +21,13 @@ function solution(land) {
             visited[x][y] = VISIT;
             
             if (land[x][y] === 1) {
-                q.push([x, y]);
+                stack.push([x, y]);
                 
                 let oilArea = 0;
                 const oilCoords = new Set();
                 
-                while (q.length) {
-                    const [oilX, oilY] = q.pop();
+                while (stack.length) {
+                    const [oilX, oilY] = stack.pop();
                     
                     if (land[oilX][oilY] === 1) {
                         oilArea++;
@@ -37,17 +42,17 @@ function solution(land) {
                                   movedX >= n ||
                                   movedY >= m;
                             
-                            if (isOutOfBoundary || visited[movedX][movedY] === VISIT) continue;
+                            if (isOutOfBoundary ||
+                                visited[movedX][movedY] === VISIT) continue;
                             
                             visited[movedX][movedY] = VISIT;
                             
                             if (land[movedX][movedY] === 1) {
-                                q.push([movedX, movedY]);
+                                stack.push([movedX, movedY]);
                             }
                         }
                     }
                 }
-                
                 
                 for (const sand of Array.from(oilCoords)) {
                     oilAreaByCoords[sand] = (oilAreaByCoords[sand] || 0) + oilArea;
