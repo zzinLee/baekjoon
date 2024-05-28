64c0144a5dd70b35fba5c97361343baeacec0a1c
Sujin Lee <91821887+zzinLee@users.noreply.github.com>
2024-05-28 14:46:27 +0900

    [level 2] Title: 타겟 넘버, Time: 1.13 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,20 +1,19 @@
 function solution(numbers, target) {
-    let sumArr = [numbers[0], -1 * numbers[0]];
-    let curIndex = 1;
-
-    while (curIndex < numbers.length) {
-        const newArr = [];
-        
-        while (sumArr.length) {
-            const cur = sumArr.pop();
-            const plus = cur + numbers[curIndex];
-            const minus = cur - numbers[curIndex];
-            newArr.push(plus, minus);
+    let ans = 0;
+    
+    search(0, 0);
+    
+    function search(index, sum) {
+        if (index < numbers.length) {
+            search(index + 1, sum + numbers[index]);
+            search(index + 1, sum - numbers[index]);
+        } else {
+            if (sum === target) {
+                ans++;
+            }
         }
-
-        sumArr = newArr;
-        curIndex++;
     }
     
-    return sumArr.filter((v) => v === target).length;
+    return ans;
 }
+
