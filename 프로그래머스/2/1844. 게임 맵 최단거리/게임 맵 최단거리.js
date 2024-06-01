0cd689b415df91e8a966165ed8289a8d7c89c228
Sujin Lee <91821887+zzinLee@users.noreply.github.com>
2024-06-01 23:21:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.51 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,33 +1,33 @@
 function solution(maps) {
-    const n = maps.length;
-    const m = maps[0].length;
-    const moveX = [-1, 1, 0, 0];
-    const moveY = [0, 0, -1, 1];
-    const q = [[0, 0, 1]];
-    maps[0][0] = 0;
-    
-    while (q.length) {
-        const [x, y, count] = q.shift();
-        
-        if (x === n - 1 && y === m - 1) {
-            return count;
-        }
-        
-        for (let i = 0; i < 4; i++) {
-            const nx = x + moveX[i];
-            const ny = y + moveY[i];
-            const isOut = nx < 0 || ny < 0 || nx >= n; ny >= m;
-            
-            if (isOut || !maps[nx][ny]) {
-                continue;
-            }
-            
-            maps[nx][ny] = 0;
-
-            q.push([nx, ny, count + 1]);
-        }
-    }
-    
-    return -1;
+	const n = maps.length;
+	const m = maps[0].length;
+	const movesX = [-1, 1, 0, 0];
+	const movesY = [0, 0, -1, 1];
+	const q = [[0, 0, 1]]; //좌표X,좌표Y,Count
+	
+	maps[0][0] = 0; //방문표기
+	
+	while (q.length) {
+		const [x, y, count] = q.shift(); //BFS순회, 최단거리를 구하므로
+		
+		if (x === n - 1 && y === m - 1) {
+			return count;
+		}
+		
+		for (let i = 0; i < 4; i++) {
+			const newX = x + movesX[i];
+			const newY = y + movesY[i];
+			
+			if (newX < 0 || newY < 0 || newX >= n || newY >= m
+				|| !maps[newX][newY]) {
+				continue;
+			}
+	
+			maps[newX][newY] = 0;
+			
+			q.push([newX, newY, count + 1]);
+		}
+	}
+	
+	return -1;	
 }
-
