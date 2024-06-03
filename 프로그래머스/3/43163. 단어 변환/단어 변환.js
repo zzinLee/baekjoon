bf9b125532899553075646a63773d06cd94573f8
Sujin Lee <91821887+zzinLee@users.noreply.github.com>
2024-06-03 15:42:50 +0900

    [level 3] Title: 단어 변환, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -11,22 +11,26 @@ const isChangable = (word, changeWord) => {
 };
 
 function solution(begin, target, words) {
-    const visited = { [begin] : 0 };
-    const q = [begin];
+    const set = new Set();
+    const answer = [];
+
+    DFS(begin, 0, set);
     
-    while (q.length) {
-        const cur = q.shift();
+    function DFS(current, count, visited) {
+        if (current === target) {
+            answer.push(count);
+            
+            return;
+        }
         
-        if (cur === target) break;
-        
-        for (const word of words) {
-            if (!visited[word] && isChangable(word, cur)) {
-                visited[word] = visited[cur] + 1;
-                
-                q.push(word);
+        visited.add(current);
+
+        for (let i = 0; i < words.length; i++) {
+            if (!visited.has(words[i]) && isChangable(current, words[i])) {
+                DFS(words[i], count + 1, visited);
             }
         }
     }
     
-    return visited[target] ? visited[target] : 0;
+    return answer.length ? Math.min(...answer) : 0; 
 }
