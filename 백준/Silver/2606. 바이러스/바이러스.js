12155544ed948e28a1e6c2ffe6b481ec4cf80fed
Sujin Lee <91821887+zzinLee@users.noreply.github.com>
2024-06-28 10:50:04 +0900

    [Silver III] Title: 바이러스, Time: 116 ms, Memory: 9476 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,52 +1,44 @@
-let fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const INPUT_PATH = "../inputs/바이러스.txt";
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : INPUT_PATH;
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-function solution(input) {
-  //input 가공
-  const [a, _, ...c] = input;
-  const computers = Number(a);
-  const connectedPairs = c.map((val) => val.split(" ").map((val) => Number(val) - 1));
-  const connectGraph = new Array(computers).fill(0).map(() => new Array(computers).fill(0));
-  const zombieComputers = new Set();
+const total = Number(input.shift());
+input.shift();
+const links = input.map((value) => value.split(" ").map(Number));
 
-  //연결된 컴퓨터는 1로, 아니면 0으로 나타내는 그래프 작성
-  for (const [c1, c2] of connectedPairs) {
-    connectGraph[c1][c2] = 1;
-    connectGraph[c2][c1] = 1;
+function solution(total, links) {
+  const computers = new Array(total + 1).fill(0).map(() => new Set());
+
+  for (const [start, end] of links) {
+    computers[start].add(end);
+    computers[end].add(start);
   }
 
-  //1번 컴퓨터(0)를 시작으로 감염
-  const start = 0;
-  const queue = [start];
-  zombieComputers.add(start);
+  const virus = 1;
+  const visited = new Set();
 
-  //1번 컴퓨터 제외하고 감염된 컴퓨터를 센다.
-  let zombies = 0;
+  let count = 0;
+  const q = [virus];
 
-  while (queue.length) {
-    const zombieComputer = queue.shift();
+  while (q.length) {
+    const virusComputer = q.shift();
 
-    connectGraph[zombieComputer].forEach((connectionOn, connectedComputer) => {
-      //좀비 컴퓨터에 연결되었는지, connectionOn === 1
-      //좀비 컴퓨터목록에 없는지, !zombieComputers.has(connectedComputer)
-      if (connectionOn === 1 && !zombieComputers.has(connectedComputer)) {
-        zombies++;
+    visited.add(virusComputer);
 
-        //다음으로 확인 할 컴퓨터
-        queue.push(connectedComputer);
+    const connected = Array.from(computers[virusComputer]);
 
-        //좀비 컴퓨터 목록 zombieComputers에 추가
-        zombieComputers.add(connectedComputer);
-
-        //연결 관계 정리
-        connectGraph[zombieComputer][connectedComputer] = 0;
-        connectGraph[connectedComputer][zombieComputer] = 0;
+    for (const connectedComputer of connected) {
+      if (!visited.has(connectedComputer)) {
+        q.push(connectedComputer);
+        visited.add(connectedComputer);
+        count++;
       }
-    });
+    }
   }
 
-  return zombies;
+  return count;
 }
 
-console.log(solution(input));
+console.log(solution(total, links));
